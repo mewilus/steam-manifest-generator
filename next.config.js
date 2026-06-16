f1bb8bba41c3df93864a1b72aba8cf99775652fb
mewilus <umar_rauf2013@yahoo.com>
2026-06-16 12:34:24 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client']
-  },
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
